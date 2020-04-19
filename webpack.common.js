797d564b708ebe824c83df637bf483a147909016
masaki_yamanaka <ki.ymnka9@gmail.com>
2020-04-19 12:51:39 +0900

    Initial commit

---
+++ webpack.common.js
@@ -0,0 +1,18 @@
+const path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: "./src/template.html"
+  })],
+};
